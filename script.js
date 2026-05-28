24250cfad1cee5402290c88566b0cb19101d1bed
lesterRx <castillobradley291@gmail.com>
2026-05-28 14:50:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// MAS VENDIDOS
+
+document.getElementById("img1").src =
+"sauvage.jpg";
+
+document.getElementById("img2").src =
+"good girl.jpg";
+
+document.getElementById("img3").src =
+"versace eros.jpg";
+
+
+// RECOMENDADOS
+
+document.getElementById("img4").src =
+"YARA.jpg";
+
+document.getElementById("img5").src =
+"ONE MILLION LUKY (2).jpg";
+
+document.getElementById("img6").src =
+"MISS DIOR EAU DE PARFUM.jpg";
